058ba8b8468b77f9377c56abf125686f0409094d
Teukros <subotkowski@gmail.com>
2018-04-13 12:28:10 +0200

    cache without reset

--- services/cache.js
+++ services/cache.js
@@ -6,14 +6,30 @@ const client = redis.createClient(redisUrl);
 const util = require('util');
 client.get = util.promisify(client.get);
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.cache = function() {
+    this.useCache = true;
+    return this;
+};
+
+mongoose.Query.prototype.exec = async function () {
     console.log("i'm about to run a query:");
     console.log(this.getQuery());
     console.log(this.mongooseCollection.name);
 
-    const key = Object.assign(
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+
+    const key = JSON.stringify(Object.assign(
         {},
         this.getQuery(),
-        {collection: this.mongooseCollection.name});
-    return exec.apply(this, arguments);
+        {collection: this.mongooseCollection.name})
+    );
+    const cacheValue = await client.get(key);
+    if (cacheValue) {
+        const doc = JSON.parse(cacheValue);
+        return Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(doc);
+    }
+    client.set(key, JSON.stringify(result));
+    return result;
 };
