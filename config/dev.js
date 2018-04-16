0e0eee60765ab2b19414259a2164a45f8869d854
Teukros <subotkowski@gmail.com>
2018-04-16 21:24:23 +0200

    AWS image upload

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
     googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
     mongoURI: 'mongodb://adam:adam@ds125318.mlab.com:25318/blog_dev',
     cookieKey: '123123123',
-    redisUrl: 'redis://127.0.0.1:6379'
+    redisUrl: 'redis://127.0.0.1:6379',
+    accessKeyId: 'something',
+    secretAccessKey: "something"
 };
